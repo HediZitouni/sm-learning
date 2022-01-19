094723ed6567d212e68da70e458e42b21c7ced7b
HediZitouni <zitouni.hedi1@gmail.com>
2022-01-19 10:24:56 +0100

    custom_add_function, create recursive add function

--- big-numbers/src/big-numbers.ts
+++ big-numbers/src/big-numbers.ts
@@ -2,6 +2,7 @@ import { CustomOperation } from './custom-number.types'
 
 export type BigNumber = number[]
 const MAX_AUTHORIZED_DIGITS = 15
+const MAX_BEFORE_ADDING_ONE = 10 ** MAX_AUTHORIZED_DIGITS
 
 const fromString = (s: string): BigNumber => {
   let index = s.length
@@ -18,7 +19,29 @@ const fromString = (s: string): BigNumber => {
   return bigNumber.reverse()
 }
 
-const add = (n1: BigNumber, n2: BigNumber): BigNumber => undefined
+const add = (n1: BigNumber, n2: BigNumber): BigNumber => {
+  return reccursiveAdd(n1, n2)
+}
+
+function reccursiveAdd (n1: BigNumber, n2: BigNumber, addOne = 0): BigNumber {
+  if (n1.length <= 1 && n2.length <= 1) {
+    const sum = (n1[0] || 0) + (n2[0] || 0) + addOne
+    const currentAddOne = +(sum >= MAX_BEFORE_ADDING_ONE)
+    if (currentAddOne) {
+      return [1, sum - MAX_BEFORE_ADDING_ONE]
+    } else {
+      return [sum]
+    }
+  }
+
+  const poppedN1 = n1.pop() || 0
+  const poppedN2 = n2.pop() || 0
+
+  let subSum = (poppedN1 || 0) + (poppedN2 || 0) + addOne
+  const willAddOne = +(subSum >= MAX_BEFORE_ADDING_ONE)
+  if (willAddOne) subSum -= MAX_BEFORE_ADDING_ONE
+  return [...reccursiveAdd(n1, n2, willAddOne), subSum]
+}
 
 const multiply = (n1: BigNumber, n2: BigNumber): BigNumber => undefined
 
