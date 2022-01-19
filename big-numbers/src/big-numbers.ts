267646b4f48cb265d335b04a31326d4ee79a2bbf
HediZitouni <zitouni.hedi1@gmail.com>
2022-01-19 09:43:38 +0100

    feature/custom_string_functions, creating fromString and toString function

--- big-numbers/src/big-numbers.ts
+++ big-numbers/src/big-numbers.ts
@@ -1,14 +1,34 @@
 import { CustomOperation } from './custom-number.types'
 
-export type BigNumber = any
+export type BigNumber = number[]
+const MAX_AUTHORIZED_DIGITS = 15
 
-const fromString = (s: string): BigNumber => undefined
+const fromString = (s: string): BigNumber => {
+  let index = s.length
+  const bigNumber = []
+  while (index - MAX_AUTHORIZED_DIGITS >= 0) {
+    const subNumber = s.substr(
+      index - MAX_AUTHORIZED_DIGITS,
+      MAX_AUTHORIZED_DIGITS
+    )
+    bigNumber.push(+subNumber)
+    index -= MAX_AUTHORIZED_DIGITS
+  }
+  bigNumber.push(+s.substr(0, index))
+  return bigNumber.reverse()
+}
 
 const add = (n1: BigNumber, n2: BigNumber): BigNumber => undefined
 
 const multiply = (n1: BigNumber, n2: BigNumber): BigNumber => undefined
 
-const toString = (n: BigNumber) => ''
+const toString = (n: BigNumber): string => {
+  let str = `${n[0]}`
+  for (let index = 1; index < n.length; index++) {
+    str += `${n[index]}`.padStart(MAX_AUTHORIZED_DIGITS, '0')
+  }
+  return str
+}
 
 export const BigNumberOperation: CustomOperation<BigNumber> = {
   fromString,
